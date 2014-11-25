96c1b1f32a28bab5f095bf4d40908c7be7990aa2
matyhtf <mikan.tenny@gmail.com>
2014-11-25 18:40:31 +0800

    增加注释

--- webim_server.php
+++ webim_server.php
@@ -1,9 +1,21 @@
 <?php
 define('DEBUG', 'on');
-define("WEBPATH", __DIR__);
+define('WEBPATH', __DIR__);
 
+/**
+ * /vendor/autoload.php是Composer工具生成的
+ * shell: composer update
+ */
 require __DIR__.'/vendor/autoload.php';
+
+/**
+ * Swoole框架自动载入器初始化
+ */
 Swoole\Loader::vendor_init();
+
+/**
+ * 注册命名空间到自动载入器中
+ */
 Swoole\Loader::addNameSpace('WebIM', __DIR__.'/src/');
 
 $config = require __DIR__.'/config.php';
