54fc7de05a46de8efbe9aa2e0303136d88b2c5eb
matyhtf <mikan.tenny@gmail.com>
2016-09-09 14:55:21 +0800

    兼容composer和手工下载

--- client/webserver_for_im.php
+++ client/webserver_for_im.php
@@ -1,15 +1,25 @@
 <?php
 define('DEBUG', 'on');
-define("WEBPATH", realpath(__DIR__ . '/../'));
+define('WEBPATH', __DIR__);
+define('ROOT_PATH', dirname(__DIR__));
 
-require_once __DIR__.'/../vendor/autoload.php';
-Swoole\Loader::vendor_init();
+if (is_file(ROOT_PATH.'/framework/libs/lib_config.php'))
+{
+    require ROOT_PATH.'/framework/libs/lib_config.php';
+}
+else
+{
+    require_once ROOT_PATH.'/vendor/autoload.php';
+    Swoole\Loader::vendor_init();
+}
+
 Swoole\Loader::addNameSpace('WebIM', __DIR__.'/src/');
 Swoole::$php->config->setPath(__DIR__.'/apps/configs');
 
-$AppSvr = new Swoole\Protocol\HttpServer();
-$AppSvr->loadSetting(__DIR__.'/../swoole.ini'); //加载配置文件
+$AppSvr = new Swoole\Protocol\AppServer();
+$AppSvr->loadSetting(ROOT_PATH.'/swoole.ini'); //加载配置文件
 $AppSvr->setDocumentRoot(__DIR__);
+$AppSvr->setAppPath(__DIR__.'/apps/');
 $AppSvr->setLogger(new \Swoole\Log\EchoLog(true)); //Logger
 
 $server = new \Swoole\Network\Server('0.0.0.0', 8888);
